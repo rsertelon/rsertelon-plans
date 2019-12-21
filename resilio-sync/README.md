38c422feb171907e2418b3c0eb08374481a81aa3
Romain Sertelon <romain@sertelon.fr>
2019-12-21 17:25:19 +0100

    Add resilio-sync
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

---
+++ resilio-sync/README.md
@@ -0,0 +1,16 @@
+# Resilio sync
+
+This plan packages [Resilio Sync](https://www.resilio.com/platforms/desktop/).
+
+Note that to use it, you MUST accept their terms listed in the `LICENSE.TXT` file that is bundled in the package:
+
+```
+By using this application, you agree to our Privacy Policy, Terms of Use and End User License Agreement.
+http://www.resilio.com/legal/privacy
+http://www.resilio.com/legal/terms-of-use
+http://www.resilio.com/legal/eula
+```
+
+## Usage
+
+Just load the package in the Habitat supervisor!
---
+++ resilio-sync/plan.sh
@@ -0,0 +1,34 @@
+pkg_name=resilio-sync
+pkg_origin=rsertelon
+pkg_version=2.6.3
+pkg_maintainer="Romain Sertelon <romain@sertelon.fr>"
+pkg_license=('Resilio Sync License')
+pkg_upstream_url="https://www.resilio.com/platforms/desktop/"
+pkg_source="https://download-cdn.resilio.com/stable/linux-x64/resilio-sync_x64.tar.gz"
+pkg_shasum="9f6adeaea9a6bdbdf232bc585929816aeeeb3bd654252cabd99f1edb753b9384"
+pkg_deps=(
+  core/glibc
+)
+pkg_build_deps=(
+  core/patchelf
+)
+pkg_bin_dirs=(bin)
+
+pkg_svc_user="root"
+pkg_svc_group="root"
+
+do_build() {
+  return 0
+}
+
+do_install() {
+  cp $HAB_CACHE_SRC_PATH/rslsync $pkg_prefix/bin
+  cp $HAB_CACHE_SRC_PATH/LICENSE.TXT $pkg_prefix/
+  patchelf --interpreter "$(pkg_path_for glibc)/lib/ld-linux-x86-64.so.2" \
+    --set-rpath "$LD_RUN_PATH" \
+    $pkg_prefix/bin/rslsync
+}
+
+do_strip() {
+  return 0
+}
