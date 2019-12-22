eabf4fa24ba4bfe69566e37641517b313437b11a
Romain Sertelon <romain@sertelon.fr>
2019-12-22 14:01:25 +0100

    Add ampache
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

---
+++ ampache/plan.sh
@@ -0,0 +1,36 @@
+pkg_name=ampache
+pkg_origin=rsertelon
+pkg_version="4.0.4"
+pkg_maintainer="Romain Sertelon <romain@sertelon.fr>"
+pkg_license=("AGPL-3.0")
+pkg_upstream_url="http://ampache.org/"
+pkg_description="A web based audio/video streaming application and file manager allowing you to access your music & videos from anywhere, using almost any internet enabled device."
+pkg_source="https://github.com/ampache/ampache/archive/${pkg_version}.tar.gz"
+pkg_shasum="38427ff92dbe4394aa0f9925f0bccfe61e2e54e84402792e17df7f1d33d4efbc"
+pkg_filename="${pkg_version}.tar.gz"
+
+pkg_svc_user=root
+pkg_svc_group=$pkg_svc_user
+
+pkg_deps=(
+  core/nginx
+)
+
+pkg_build_deps=(
+  core/composer
+)
+
+pkg_binds=(
+  [php]="address port"
+)
+
+do_build(){
+  return 0
+}
+
+do_install() {
+  mkdir -p "${pkg_prefix}/ampache"
+  cp -r * "${pkg_prefix}/ampache/"
+  cd $pkg_prefix/ampache
+  composer install --prefer-source --no-interaction
+}
