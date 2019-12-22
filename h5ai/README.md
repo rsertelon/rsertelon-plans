df8a9170a26b3f4f553c2bb02bc48ceeed19c276
Romain Sertelon <romain@sertelon.fr>
2019-12-22 17:27:54 +0100

    Add h5ai
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

---
+++ h5ai/README.md
@@ -0,0 +1,9 @@
+# h5ai
+
+This plan packages [h5ai](https://larsjung.de/h5ai/) a modern HTTP web server index.
+
+## Usage
+
+This package is intended as a dependency only, and will not run anything.
+
+You can use it to reference in an habitat nginx configuration with `$(pkg_path_for "rsertelon/h5ai")`
---
+++ h5ai/plan.sh
@@ -0,0 +1,20 @@
+pkg_name=h5ai
+pkg_origin=rsertelon
+pkg_version=0.29.2
+pkg_maintainer="Romain Sertelon <romain@sertelon.fr>"
+pkg_license=('MIT')
+pkg_description="modern HTTP web server index"
+pkg_upstream_url="https://larsjung.de/h5ai/"
+pkg_source="https://release.larsjung.de/h5ai/h5ai-${pkg_version}.zip"
+pkg_shasum="612acd8cc724ab42c5266f7b9966a0212b561331d8770b5609891edb5cb69a8b"
+pkg_dirname="_h5ai"
+
+do_build() {
+    return 0
+}
+
+do_install() {
+    dest="${pkg_prefix}/_h5ai"
+    mkdir $dest
+    cp -R * $dest
+}
