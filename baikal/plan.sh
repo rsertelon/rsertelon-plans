aff7a96a48ee16b4e4c37864c9b9aca10cf4170c
Romain Sertelon <romain@sertelon.fr>
2019-12-26 01:29:11 +0100

    baikal: new package
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

---
+++ baikal/plan.sh
@@ -0,0 +1,30 @@
+pkg_name=baikal
+pkg_origin=rsertelon
+pkg_version="0.6.1"
+pkg_maintainer="Romain Sertelon <romain@sertelon.fr>"
+pkg_license=("GPL-3.0")
+pkg_description="Baïkal is a Cal and CardDAV server, based on sabre/dav, that includes an administrative interface for easy management."
+pkg_upstream_url="http://sabre.io/baikal/"
+pkg_source="https://github.com/sabre-io/Baikal/releases/download/${pkg_version}/baikal-${pkg_version}.zip"
+pkg_shasum="b3a553591a84f4262a6333cf79a1624e5d424d52c4071c77934b7191f714d757"
+pkg_dirname="baikal"
+
+pkg_svc_user=root
+pkg_svc_group=$pkg_svc_user
+
+pkg_deps=(
+  core/nginx
+)
+
+pkg_binds=(
+  [php]="address port"
+)
+
+do_build(){
+  return 0
+}
+
+do_install() {
+  mkdir -p "${pkg_prefix}/baikal/"
+  cp -r * "${pkg_prefix}/baikal/"
+}
