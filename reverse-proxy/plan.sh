c9c39e3b58027fe8e352343b6d2580810816f515
Romain Sertelon <romain@sertelon.fr>
2020-04-08 22:20:54 +0200

    Add reverse-proxy package
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

---
+++ reverse-proxy/plan.sh
@@ -0,0 +1,21 @@
+pkg_name=reverse-proxy
+pkg_origin=rsertelon
+pkg_version="0.1.0"
+pkg_maintainer="Romain Sertelon <romain@sertelon.fr>"
+pkg_license=("GPL-3.0")
+pkg_description="TLS Reverse proxy for multiple services"
+
+pkg_svc_user=root
+pkg_svc_group=$pkg_svc_user
+
+pkg_deps=(
+  core/nginx
+)
+
+do_build(){
+  return 0
+}
+
+do_install() {
+  return 0
+}
