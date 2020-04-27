7eaedd69a81f40d3e60d3d7c8de13a98415e3d3a
Romain Sertelon <romain@sertelon.fr>
2020-04-27 23:43:43 +0200

    [ampache] Update to 4.1.1
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- ampache/plan.sh
+++ ampache/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=ampache
 pkg_origin=rsertelon
-pkg_version="4.0.4"
+pkg_version="4.1.1"
 pkg_maintainer="Romain Sertelon <romain@sertelon.fr>"
 pkg_license=("AGPL-3.0")
 pkg_upstream_url="http://ampache.org/"
 pkg_description="A web based audio/video streaming application and file manager allowing you to access your music & videos from anywhere, using almost any internet enabled device."
 pkg_source="https://github.com/ampache/ampache/archive/${pkg_version}.tar.gz"
-pkg_shasum="38427ff92dbe4394aa0f9925f0bccfe61e2e54e84402792e17df7f1d33d4efbc"
+pkg_shasum="48ff858484194a4949702946ccad542316f1c7c67045193167f8e215ab8a6385"
 pkg_filename="${pkg_version}.tar.gz"
 
 pkg_svc_user=root
@@ -18,6 +18,7 @@ pkg_deps=(
 
 pkg_build_deps=(
   core/composer
+  core/patch
 )
 
 pkg_binds=(
@@ -32,5 +33,7 @@ do_install() {
   mkdir -p "${pkg_prefix}/ampache"
   cp -r * "${pkg_prefix}/ampache/"
   cd $pkg_prefix/ampache
+
+  patch -p1 -i "$PLAN_CONTEXT/patches/dropbox-fix.patch"
   composer install --prefer-source --no-interaction
 }
