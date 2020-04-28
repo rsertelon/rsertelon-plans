d09a00a31c14213b2129fd40d81fa583c2533c19
Romain Sertelon <romain@sertelon.fr>
2020-04-28 21:07:28 +0200

    [phpmyadmin] Update to 4.9.5
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- phpmyadmin/plan.sh
+++ phpmyadmin/plan.sh
@@ -1,14 +1,14 @@
 pkg_origin=rsertelon
 pkg_name=phpmyadmin
-pkg_version="4.9.2"
-pkg_maintainer="Romain Sertelon <romain@sertelon.fr>"
-pkg_license=("GPL-2.0")
-pkg_source="https://files.phpmyadmin.net/phpMyAdmin/${pkg_version}/phpMyAdmin-${pkg_version}-all-languages.zip"
-pkg_filename="phpMyAdmin-${pkg_version}-all-languages.zip"
-pkg_dirname="phpMyAdmin-${pkg_version}-all-languages"
+pkg_version="4.9.5"
 pkg_description="phpMyAdmin is a free software tool written in PHP, intended to handle the administration of MySQL over the Web."
 pkg_upstream_url="https://www.phpmyadmin.net/"
-pkg_shasum="82c2bb365804168f61d32d4bf7a00afeaf2509aaedd7e79433c705e67fa0a4ca"
+pkg_maintainer="Romain Sertelon <romain@sertelon.fr>"
+pkg_license=("GPL-2.0")
+pkg_dirname="phpMyAdmin-${pkg_version}-all-languages"
+pkg_filename="${pkg_dirname}.tar.xz"
+pkg_source="https://files.phpmyadmin.net/phpMyAdmin/${pkg_version}/${pkg_filename}"
+pkg_shasum="e02823e7844bc17aa6393e1acfed6970f5a3688fe8d0c693e74670d8fed9ecd4"
 
 pkg_svc_user=root
 pkg_svc_group=$pkg_svc_user
