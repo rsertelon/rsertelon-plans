f4ee95d5b9e5d8a77110338e29f54ba24a4ff20a
Romain Sertelon <romain@sertelon.fr>
2020-06-13 00:52:26 +0200

    Add cronie package
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

---
+++ cronie/plan.sh
@@ -0,0 +1,33 @@
+pkg_name=cronie
+pkg_origin=rsertelon
+pkg_version="1.5.5"
+pkg_upstream_url="https://github.com/cronie-crond/cronie"
+pkg_description="Cronie contains the standard UNIX daemon crond that runs specified programs at scheduled times and related tools."
+pkg_maintainer="Romain Sertelon <romain@sertelon.fr>"
+pkg_license=('GPL-2')
+pkg_source="https://github.com/cronie-crond/cronie/releases/download/cronie-${pkg_version}/cronie-${pkg_version}.tar.gz"
+pkg_shasum="be34c79505e5544323281854744b9955ff16b160ee569f9df7c0dddae5720eac"
+pkg_deps=(
+  core/glibc
+)
+pkg_build_deps=(
+  core/gcc
+  core/make
+)
+pkg_bin_dirs=(bin sbin)
+
+pkg_svc_user="root"
+pkg_svc_group="root"
+
+do_build() {
+  SPOOL_DIR="${pkg_svc_data_path}/crontabs" \
+  ./configure --prefix="${pkg_prefix}" \
+    --disable-syscrontab \
+    --disable-anacron \
+    --enable-pie \
+    --enable-relro \
+    --with-daemon_username=hab \
+    --with-daemon_groupname=hab
+
+  make
+}
