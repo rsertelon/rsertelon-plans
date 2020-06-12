8a14939c5261cee573a32db9d3bf6fe4c3ff303e
Romain Sertelon <romain@sertelon.fr>
2020-06-12 23:45:28 +0200

    Add rclone package
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

---
+++ rclone/plan.sh
@@ -0,0 +1,24 @@
+pkg_name=rclone
+pkg_origin=rsertelon
+pkg_version=1.52.1
+pkg_maintainer='Romain Sertelon <romain@sertelon.fr>'
+pkg_license='MIT'
+pkg_upstream_url='https://rclone.org/'
+pkg_description='Rclone syncs your files to cloud storage'
+pkg_dirname="rclone-v${pkg_version}-linux-amd64"
+pkg_filename="${pkg_dirname}.zip"
+pkg_source="https://downloads.rclone.org/v${pkg_version}/${pkg_filename}"
+pkg_shasum="2fbee69267f5242dbc20ba9e0834d06f1330bd3a7fda2bc04dddd8abdd8516a2"
+pkg_bin_dirs=(bin)
+
+do_build() {
+  return 0
+}
+
+do_install() {
+  cp rclone ${pkg_prefix}/bin
+}
+
+do_strip() {
+  return 0
+}
