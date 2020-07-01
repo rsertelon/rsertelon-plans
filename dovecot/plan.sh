4012a89900b9b6ac8e79d74917673d21cd696ce9
Romain Sertelon <romain@sertelon.fr>
2020-07-01 20:20:41 +0200

    [dovecot] include pigeonhole plugins in package
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- dovecot/plan.sh
+++ dovecot/plan.sh
@@ -8,6 +8,10 @@ pkg_license=("LGPL-2.1" "MIT")
 pkg_source="https://dovecot.org/releases/2.3/dovecot-${pkg_version}.tar.gz"
 pkg_shasum="6642e62f23b1b23cfac235007ca6e21cb67460cca834689fad450724456eb10c"
 
+pigeonhole_version=0.5.10
+pigeonhole_dirname="dovecot-2.3-pigeonhole-${pigeonhole_version}"
+pigeonhole_filename="${pigeonhole_dirname}.tar.gz"
+
 pkg_deps=(
   core/bzip2
   core/gcc-libs
@@ -34,6 +38,24 @@ pkg_lib_dirs=(lib)
 pkg_svc_user="root"
 pkg_svc_group="root"
 
+do_download() {
+  do_default_download
+
+  download_file "https://pigeonhole.dovecot.org/releases/2.3/${pigeonhole_filename}" $pigeonhole_filename 48c89cc9f3caa9c5f2454f9dcca74fe251a99749a38062bfab7e5017d329605e
+}
+
+do_clean() {
+  do_default_clean
+
+  rm -rf "$HAB_CACHE_SRC_PATH/${pigeonhole_dirname}"
+}
+
+do_unpack() {
+  do_default_unpack
+
+  tar xf "$HAB_CACHE_SRC_PATH/${pigeonhole_filename}"
+}
+
 do_prepare() {
   export CFLAGS="${CFLAGS} -O2"
   export CXXFLAGS="${CXXFLAGS} -O2"
@@ -45,11 +67,30 @@ do_prepare() {
 }
 
 do_build() {
+  # build dovecot
   ./configure --prefix=${pkg_prefix} \
     --with-mysql \
     --with-sql
 
   make -j$(nproc)
+
+  # build pigeonhole
+  pushd "$HAB_CACHE_SRC_PATH/${pigeonhole_dirname}"
+    ./configure --prefix=${pkg_prefix} \
+      --with-dovecot=../$pkg_dirname/
+
+    make -j$(nproc)
+  popd
+}
+
+do_install() {
+  # install dovecot
+  do_default_install
+
+  # install pigeonhole
+  pushd "$HAB_CACHE_SRC_PATH/${pigeonhole_dirname}"
+    make install
+  popd
 }
 
 do_end() {
