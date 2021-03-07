29066c3be7ab09c3cc40210e82b5bdacfefebd39
Romain Sertelon <romain@sertelon.fr>
2021-03-07 21:39:45 +0100

    Update umurmur to 0.2.19
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- umurmur/plan.sh
+++ umurmur/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=umurmur
 pkg_origin=rsertelon
-pkg_version="0.2.17"
+pkg_version="0.2.19"
 pkg_maintainer="Romain Sertelon <romain@sertelon.fr>"
 pkg_license=("BSD-3-Clause")
 pkg_upstream_url="https://umurmur.net/"
 pkg_description="A minimalistic Mumble server primarily targeted to run on embedded computers"
 pkg_source="https://github.com/umurmur/umurmur/archive/${pkg_version}.tar.gz"
-pkg_shasum="e77b7b6616768f4a1c07442afe49a772692f667b00c23cc85909d4dd0ce206d2"
+pkg_shasum="338053160bc48e48850061cdfc19cf1b2bb66e56877c04cd6de7831b468646b6"
 
 pkg_build_deps=(
   core/cmake
