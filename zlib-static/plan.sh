5eaab3c9a3f726613026f48c1c7b2352f04782fa
Romain Sertelon <romain@sertelon.fr>
2021-05-01 00:38:06 +0200

    [zlib-static] Update to 1.2.11
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- zlib-static/plan.sh
+++ zlib-static/plan.sh
@@ -1,24 +1,26 @@
 pkg_name=zlib-static
 pkg_origin=rsertelon
-pkg_version=1.2.8
+pkg_version=1.2.11
 pkg_maintainer="Romain Sertelon <romain@sertelon.fr>"
 pkg_license=('Zlib')
-pkg_source=http://zlib.net/fossils/zlib-${pkg_version}.tar.gz
-pkg_dirname=zlib-${pkg_version}
-pkg_shasum=36658cb768a54c1d4dec43c3116c27ed893e88b02ecfcb44f2166f9c0b7f2a0d
-pkg_build_deps=(core/make core/gcc)
+pkg_source="http://zlib.net/fossils/zlib-${pkg_version}.tar.gz"
+pkg_dirname="zlib-${pkg_version}"
+pkg_shasum="c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1"
+pkg_build_deps=(
+  core/make
+  core/gcc
+)
 pkg_lib_dirs=(lib)
 pkg_include_dirs=(include)
 
 do_build() {
-    pushd $HAB_CACHE_SRC_PATH/$pkg_dirname
-        echo `pwd`
-        ./configure --static
-        make
-    popd
+  pushd "$HAB_CACHE_SRC_PATH/$pkg_dirname"
+    ./configure --static
+    make
+  popd
 }
 
 do_install() {
-    cp $HAB_CACHE_SRC_PATH/$pkg_dirname/libz.a $pkg_prefix/lib/
-    cp $HAB_CACHE_SRC_PATH/$pkg_dirname/*.h $pkg_prefix/include/
+  cp $HAB_CACHE_SRC_PATH/$pkg_dirname/libz.a $pkg_prefix/lib/
+  cp $HAB_CACHE_SRC_PATH/$pkg_dirname/*.h $pkg_prefix/include/
 }
