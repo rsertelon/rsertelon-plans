1358ec8d85aeb9590c79a11bb8645466a4da88d7
Romain Sertelon <romain@sertelon.fr>
2021-05-01 13:40:47 +0200

    [rainloop] Update to 1.15.0
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- rainloop/plan.sh
+++ rainloop/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=rainloop
 pkg_origin=rsertelon
-pkg_version="1.14.0"
+pkg_version="1.15.0"
 pkg_maintainer="Romain Sertelon <romain@sertelon.fr>"
 pkg_license=("RainLoop Software License")
 pkg_upstream_url="http://www.rainloop.net/"
 pkg_description="Simple, modern & fast web-based email client"
 pkg_source="https://github.com/RainLoop/rainloop-webmail/releases/download/v${pkg_version}/rainloop-${pkg_version}.zip"
-pkg_shasum="906dcc7d233b64083ddd3b0665c7271408c331c9a97f94fc710081c82a79c124"
+pkg_shasum="d18f4def23bf9eed2518b6f5332098410ee33ec70887c6dc50fb5fc058e941e6"
 pkg_dirname="rainloop"
 
 pkg_build_deps=(
