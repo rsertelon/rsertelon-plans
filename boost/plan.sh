2a14c31a476eaa65b7e47d7862f19c57f5afb44d
Romain Sertelon <romain@sertelon.fr>
2021-05-01 10:40:01 +0200

    [boost] Update to 1.76.0
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- boost/plan.sh
+++ boost/plan.sh
@@ -2,12 +2,12 @@ pkg_name=boost
 pkg_origin=rsertelon
 pkg_description='Boost provides free peer-reviewed portable C++ source libraries.'
 pkg_upstream_url='http://www.boost.org/'
-pkg_version=1.73.0
+pkg_version=1.76.0
 pkg_maintainer='Romain Sertelon <romain@sertelon.fr>'
 pkg_license=('Boost Software License')
 pkg_dirname="boost_${pkg_version//./_}"
 pkg_source="https://dl.bintray.com/boostorg/release/${pkg_version}/source/${pkg_dirname}.tar.gz"
-pkg_shasum="9995e192e68528793755692917f9eb6422f3052a53c5e13ba278a228af6c7acf"
+pkg_shasum="7bd7ddceec1a1dfdcbdb3e609b60d01739c38390a5f956385a12f3122049f0ca"
 
 pkg_deps=(
   core/glibc
