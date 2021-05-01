fe329063beb51e9e8788d62f5cd11967ad374e58
Romain Sertelon <romain@sertelon.fr>
2021-05-02 00:40:12 +0200

    [postfixadmin] Update to 3.3.8
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- postfixadmin/plan.sh
+++ postfixadmin/plan.sh
@@ -1,13 +1,14 @@
 pkg_name=postfixadmin
 pkg_origin=rsertelon
-pkg_version="3.2.4"
+pkg_version="3.3.8"
 pkg_maintainer="Romain Sertelon <romain@sertelon.fr>"
 pkg_license=("GPL-2.0+")
 pkg_upstream_url="http://postfixadmin.sourceforge.net/"
 pkg_description="Postfix Admin is a web based interface to configure and manage a Postfix based email server for many users."
-pkg_source="https://downloads.sourceforge.net/project/postfixadmin/postfixadmin/postfixadmin-${pkg_version%.*}/postfixadmin-${pkg_version}.tar.gz"
-pkg_dirname="postfixadmin-${pkg_version}"
-pkg_shasum="6d5b27012ce18b6dd257eb690d7d124781cf9ab44209511d49efb9ccd6ea27f1"
+pkg_filename="PostfixAdmin%20${pkg_version}.tar.gz"
+pkg_source="https://downloads.sourceforge.net/project/postfixadmin/postfixadmin-${pkg_version}/${pkg_filename}"
+pkg_dirname="postfixadmin-postfixadmin-7d04685"
+pkg_shasum="0a99da09a24ebe046075d2706845dc32fe96f2711c22085b29d95c6b6eaf59ed"
 
 pkg_svc_user=root
 pkg_svc_group=$pkg_svc_user
