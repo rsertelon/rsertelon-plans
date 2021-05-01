026c240735ac35be5e6a2a8a1bec5a15da60fe05
Romain Sertelon <romain@sertelon.fr>
2021-05-01 22:39:21 +0200

    [certbot] Make version explicit
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- certbot/plan.sh
+++ certbot/plan.sh
@@ -1,5 +1,6 @@
 pkg_name=certbot
 pkg_origin=rsertelon
+pkg_version=1.14.0
 pkg_maintainer='Romain Sertelon <romain@sertelon.fr>'
 pkg_license='Apache-2.0'
 pkg_upstream_url='https://certbot.eff.org'
@@ -14,20 +15,10 @@ pkg_plugins=(
 )
 pkg_bin_dirs=(bin)
 
-pkg_version() {
-  pip --disable-pip-version-check search "$pkg_name" \
-    | grep "^$pkg_name " \
-    | cut -d'(' -f2 \
-    | cut -d')' -f1
-}
-
-do_before() {
-  update_pkg_version
-}
-
 do_prepare() {
   python -m venv "$pkg_prefix"
   source "${pkg_prefix}/bin/activate"
+  python -m pip install -U pip
 }
 
 do_build() {
@@ -37,7 +28,7 @@ do_build() {
 do_install() {
   for plugin in ${pkg_plugins[@]}
   do
-    pip --disable-pip-version-check install "$pkg_name-$plugin==$pkg_version"
+    pip install "$pkg_name-$plugin==$pkg_version"
   done
 }
 
