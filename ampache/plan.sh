8b092cf83154810a087eea8b6169037b089a0feb
Romain Sertelon <romain@sertelon.fr>
2021-05-01 14:03:25 +0200

    [ampache] Update to 4.4.1
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- ampache/plan.sh
+++ ampache/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=ampache
 pkg_origin=rsertelon
-pkg_version="4.2.3"
+pkg_version="4.4.1"
 pkg_maintainer="Romain Sertelon <romain@sertelon.fr>"
 pkg_license=("AGPL-3.0")
 pkg_upstream_url="http://ampache.org/"
 pkg_description="A web based audio/video streaming application and file manager allowing you to access your music & videos from anywhere, using almost any internet enabled device."
 pkg_source="https://github.com/ampache/ampache/archive/${pkg_version}.tar.gz"
-pkg_shasum="0bb122111bf5f1726fa45b42028bcc93bcf46661e5cb0e08f03082c368ab6642"
+pkg_shasum="7ff3f8ae4eb18709adc4b91477d083c0f306cec85183290deedb2aacdf4809e8"
 pkg_filename="${pkg_version}.tar.gz"
 
 pkg_svc_user=root
@@ -18,7 +18,6 @@ pkg_deps=(
 
 pkg_build_deps=(
   core/composer
-  core/patch
 )
 
 pkg_binds=(
