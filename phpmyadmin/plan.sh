e404ad62349a13693f83fa96fd6ecf2141271cdf
Romain Sertelon <romain@sertelon.fr>
2021-05-01 10:29:13 +0200

    [phpmyadmin] Update to 5.1.0
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- phpmyadmin/plan.sh
+++ phpmyadmin/plan.sh
@@ -1,6 +1,6 @@
 pkg_origin=rsertelon
 pkg_name=phpmyadmin
-pkg_version="5.0.4"
+pkg_version="5.1.0"
 pkg_description="phpMyAdmin is a free software tool written in PHP, intended to handle the administration of MySQL over the Web."
 pkg_upstream_url="https://www.phpmyadmin.net/"
 pkg_maintainer="Romain Sertelon <romain@sertelon.fr>"
@@ -8,7 +8,7 @@ pkg_license=("GPL-2.0")
 pkg_dirname="phpMyAdmin-${pkg_version}-all-languages"
 pkg_filename="${pkg_dirname}.tar.xz"
 pkg_source="https://files.phpmyadmin.net/phpMyAdmin/${pkg_version}/${pkg_filename}"
-pkg_shasum="1578c1a08e594da4f4f62e676ccbdbd17784c3de769b094ba42c35bf05c057db"
+pkg_shasum="aa8ccf357f672012384df34e1c2bc70147476761c8458a0dad6233497e142c68"
 
 pkg_svc_user=root
 pkg_svc_group=$pkg_svc_user
