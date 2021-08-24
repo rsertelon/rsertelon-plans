b0ff356484b0c469f570c989de3e6988c0808ea3
Romain Sertelon <romain@sertelon.fr>
2021-08-24 21:07:58 +0200

    [rclone] Update to 1.56.0
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- rclone/plan.sh
+++ rclone/plan.sh
@@ -1,6 +1,6 @@
 pkg_name=rclone
 pkg_origin=rsertelon
-pkg_version=1.55.1
+pkg_version=1.56.0
 pkg_maintainer='Romain Sertelon <romain@sertelon.fr>'
 pkg_license='MIT'
 pkg_upstream_url='https://rclone.org/'
@@ -8,7 +8,7 @@ pkg_description='Rclone syncs your files to cloud storage'
 pkg_dirname="rclone-v${pkg_version}-linux-amd64"
 pkg_filename="${pkg_dirname}.zip"
 pkg_source="https://downloads.rclone.org/v${pkg_version}/${pkg_filename}"
-pkg_shasum="d8e28bded459511fc27e300d88c4bee0fda36e7e6222d6d9b9a32e5986163881"
+pkg_shasum="d23d0c1f295a7399114b9a07fa987e7dc216dbe989b5d88530eb01d3c87c9c1f"
 pkg_bin_dirs=(bin)
 
 do_build() {
