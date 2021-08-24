f783009feb9153bd1d9d6a5d2c293d0df8d1f0fb
Romain Sertelon <romain@sertelon.fr>
2021-08-24 21:52:23 +0200

    [boost-build] Update to 1.77.0
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- boost-build/plan.sh
+++ boost-build/plan.sh
@@ -2,12 +2,12 @@ pkg_name=boost-build
 pkg_origin=rsertelon
 pkg_description='Boost.Build is a text-based system for developing, testing, and installing software'
 pkg_upstream_url='http://www.boost.org/'
-pkg_version=1.76.0
+pkg_version=1.77.0
 pkg_maintainer='Romain Sertelon <romain@sertelon.fr>'
 pkg_license=('Boost Software License')
 pkg_dirname="boost_${pkg_version//./_}"
-pkg_source="https://dl.bintray.com/boostorg/release/${pkg_version}/source/${pkg_dirname}.tar.gz"
-pkg_shasum="7bd7ddceec1a1dfdcbdb3e609b60d01739c38390a5f956385a12f3122049f0ca"
+pkg_source="https://boostorg.jfrog.io/artifactory/main/release/${pkg_version}/source/${pkg_dirname}.tar.gz"
+pkg_shasum="5347464af5b14ac54bb945dc68f1dd7c56f0dad7262816b956138fc53bcc0131"
 
 pkg_deps=(
   core/glibc
