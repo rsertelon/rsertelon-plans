fffdc0712485befb38784d7ffff6846783e371ae
Romain Sertelon <romain@sertelon.fr>
2021-09-09 20:40:50 +0200

    [ampache] Update to 5.0.0
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- ampache/plan.sh
+++ ampache/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=ampache
 pkg_origin=rsertelon
-pkg_version="4.4.3"
+pkg_version="5.0.0"
 pkg_maintainer="Romain Sertelon <romain@sertelon.fr>"
 pkg_license=("AGPL-3.0")
 pkg_upstream_url="http://ampache.org/"
 pkg_description="A web based audio/video streaming application and file manager allowing you to access your music & videos from anywhere, using almost any internet enabled device."
 pkg_source="https://github.com/ampache/ampache/archive/${pkg_version}.tar.gz"
-pkg_shasum="978eb6e82fca9a632039f5498e0e391e65549e92a1d9bb9ff98ac20971ce12e9"
+pkg_shasum="ed1deea622385b81e3d4078e7152f5b35489dcb148e64ee6e4a8a1289fc5c950"
 pkg_filename="${pkg_version}.tar.gz"
 
 pkg_svc_user=root
@@ -17,7 +17,7 @@ pkg_deps=(
 )
 
 pkg_build_deps=(
-  core/composer
+  rsertelon/composer
 )
 
 pkg_binds=(
