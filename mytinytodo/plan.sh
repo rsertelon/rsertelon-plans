87fde2fb2f11648b2e377f92226fecada20c5508
Romain Sertelon <romain@sertelon.fr>
2021-09-24 21:22:10 +0200

    [mytinytodo] Update to 1.6.7
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- mytinytodo/plan.sh
+++ mytinytodo/plan.sh
@@ -1,6 +1,6 @@
 pkg_name=mytinytodo
 pkg_origin=rsertelon
-pkg_version="1.6.5"
+pkg_version="1.6.7"
 pkg_maintainer="Romain Sertelon <romain@sertelon.fr>"
 pkg_license=("GPL-2.0+")
 pkg_upstream_url="https://www.mytinytodo.net"
@@ -8,7 +8,7 @@ pkg_description="Simple way to manage your todo list in AJAX style. "
 pkg_filename="mytinytodo-v${pkg_version}.zip"
 pkg_source="https://github.com/maxpozdeev/mytinytodo/releases/download/v${pkg_version}/mytinytodo-v${pkg_version}.zip"
 pkg_dirname="mytinytodo"
-pkg_shasum="5a4edb2dc75f89e6aedf85f174c9d06a30b8e1fd4dbec317bcad15213eeb7223"
+pkg_shasum="1e1bc65a4ba27eecdff71a85c8e292ef54185cdc9108e1e9507f7243df1a0157"
 
 pkg_svc_user=root
 pkg_svc_group=$pkg_svc_user
