f4fa5ad5786bf166ccda9a84344beef70b94379a
Romain Sertelon <romain@sertelon.fr>
2022-02-17 00:18:03 +0100

    [wiki-js] Fix download url
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- wiki-js/plan.sh
+++ wiki-js/plan.sh
@@ -5,8 +5,8 @@ pkg_maintainer="Romain Sertelon <romain@sertelon.fr>"
 pkg_license=("AGPL-3.0")
 pkg_upstream_url="https://js.wiki/"
 pkg_description="The most powerful and extensible open source Wiki software."
-pkg_source="https://github.com/Requarks/wiki/releases/download/${pkg_version}/wiki-js.tar.gz"
-pkg_shasum="c73c4ebf5ec1fbdc1e82d3571abf520f82945a531baef3ed661bdf9a866104d7"
+pkg_source="https://github.com/Requarks/wiki/releases/download/v${pkg_version}/wiki-js.tar.gz"
+pkg_shasum="9c8e85737dc4d339cb3f40ff0ea16fcb729047346dde021b41ac8ccc3c60d8d4"
 
 pkg_svc_user=root
 pkg_svc_group=$pkg_svc_user
