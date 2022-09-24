14ad56d78a4ba700d329db6fe9873ec609efacb3
Romain Sertelon <romain@sertelon.fr>
2022-09-24 22:39:44 +0200

    [agendav] Update to 2.5.0
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- agendav/plan.sh
+++ agendav/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=agendav
 pkg_origin=rsertelon
-pkg_version="2.4.0"
+pkg_version="2.5.0"
 pkg_maintainer="Romain Sertelon <romain@sertelon.fr>"
 pkg_license=("GPL-3.0")
 pkg_description="AgenDAV is an open source multilanguage CalDAV web client which features a rich AJAX interface with shared calendars support"
 pkg_upstream_url="https://github.com/agendav/agendav"
 pkg_source="https://github.com/agendav/agendav/releases/download/${pkg_version}/agendav-${pkg_version}.tar.gz"
-pkg_shasum="02b7c88cd5353503cc36f26b0b55f0aa72e17b680f41f907a59d4815403ff98b"
+pkg_shasum="b0c4c12fdc4272de3e0296403eeea0e5b22af1e83ab6becf7a1eb0eea9e9f48e"
 
 pkg_svc_user=root
 pkg_svc_group=$pkg_svc_user
