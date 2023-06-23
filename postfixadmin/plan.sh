a53d351e56e210e6ceab88459bfdf569fb398a07
Romain Sertelon <romain@sertelon.fr>
2023-06-24 01:55:57 +0200

    [postfixadmin] Update to 3.3.13
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- postfixadmin/plan.sh
+++ postfixadmin/plan.sh
@@ -1,13 +1,13 @@
 pkg_name=postfixadmin
 pkg_origin=rsertelon
-pkg_version="3.3.12"
+pkg_version="3.3.13"
 pkg_maintainer="Romain Sertelon <romain@sertelon.fr>"
 pkg_license=("GPL-2.0+")
 pkg_upstream_url="http://postfixadmin.sourceforge.net/"
 pkg_description="Postfix Admin is a web based interface to configure and manage a Postfix based email server for many users."
 pkg_source="https://github.com/postfixadmin/postfixadmin/archive/refs/tags/postfixadmin-${pkg_version}.tar.gz"
 pkg_dirname="postfixadmin-postfixadmin-${pkg_version}"
-pkg_shasum="906d9d9eba7140562445e3e4009b50db8d05a178f4f685fbfa8230a2b95c901b"
+pkg_shasum="026c4f370656b96b6c9f62328e901b9416a6e56d1c4df86249995d661498947b"
 
 pkg_svc_user=root
 pkg_svc_group=$pkg_svc_user
