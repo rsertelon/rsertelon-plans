a6f37ece0b52bd5d173e2d77318295c5efd62c36
Romain Sertelon <romain@sertelon.fr>
2023-08-12 20:17:38 +0200

    [wiki-js] Update to 2.5.300
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- wiki-js/plan.sh
+++ wiki-js/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=wiki-js
 pkg_origin=rsertelon
-pkg_version="2.5.299"
+pkg_version="2.5.300"
 pkg_maintainer="Romain Sertelon <romain@sertelon.fr>"
 pkg_license=("AGPL-3.0")
 pkg_upstream_url="https://js.wiki/"
 pkg_description="The most powerful and extensible open source Wiki software."
 pkg_source="https://github.com/Requarks/wiki/releases/download/v${pkg_version}/wiki-js.tar.gz"
-pkg_shasum="1987b4e5d6d1f11c02ccf79d782314413599e75ae833f5768d43cfbfba3b5045"
+pkg_shasum="0b272ada8781f2fd3656d13928fb34f7ebb366abc66c9447f89e183c3628fb26"
 
 pkg_svc_user=root
 pkg_svc_group=$pkg_svc_user
