b5df33ada419198f49129df5c335f3071288eb3c
Romain Sertelon <romain@sertelon.fr>
2023-11-22 23:58:36 +0100

    [dolibarr] Create package
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

---
+++ dolibarr/plan.sh
@@ -0,0 +1,29 @@
+pkg_origin=rsertelon
+pkg_name=dolibarr
+pkg_version="18.0.3"
+pkg_description="Dolibarr ERP & CRM is a modern software package that helps manage your organization's activity (contacts, suppliers, invoices, orders, stocks, agenda…)."
+pkg_upstream_url="https://www.dolibarr.org/"
+pkg_maintainer="Romain Sertelon <romain@sertelon.fr>"
+pkg_license=("GPL-3.0-or-later")
+pkg_source="https://github.com/Dolibarr/dolibarr/archive/refs/tags/${pkg_version}.tar.gz"
+pkg_shasum="8639d466b26f0fba7d2b610a96ffcf36a657b1e00bdc2e5a1ac5287521dcb4ed"
+
+pkg_svc_user=root
+pkg_svc_group=$pkg_svc_user
+
+pkg_deps=(
+  core/nginx
+)
+
+pkg_binds=(
+  [php]="address port"
+)
+
+do_build(){
+  return 0
+}
+
+do_install() {
+  mkdir -p "${pkg_prefix}/dolibarr/"
+  cp -r * "${pkg_prefix}/dolibarr/"
+}
