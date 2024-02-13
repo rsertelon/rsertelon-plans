33f5678edb85ac09a9ac45685416232242061842
Romain Sertelon <romain@sertelon.fr>
2024-02-13 22:23:49 +0100

    [cronie] Update to 1.7.1
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- cronie/plan.sh
+++ cronie/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=cronie
 pkg_origin=rsertelon
-pkg_version="1.6.1"
+pkg_version="1.7.1"
 pkg_upstream_url="https://github.com/cronie-crond/cronie"
 pkg_description="Cronie contains the standard UNIX daemon crond that runs specified programs at scheduled times and related tools."
 pkg_maintainer="Romain Sertelon <romain@sertelon.fr>"
 pkg_license=('GPL-2')
 pkg_source="https://github.com/cronie-crond/cronie/releases/download/cronie-${pkg_version}/cronie-${pkg_version}.tar.gz"
-pkg_shasum="2cd0f0dd1680e6b9c39bf1e3a5e7ad6df76aa940de1ee90a453633aa59984e62"
+pkg_shasum="78033100c24413f0c40f93e6138774d6a4f55bc31050567b90db45a2f9f1b954"
 pkg_deps=(
   core/glibc
 )
