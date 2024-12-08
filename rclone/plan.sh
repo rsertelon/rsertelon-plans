a63ca7a2b380eb8a329e82914ed11649957ff922
Romain Sertelon <romain@sertelon.fr>
2024-12-09 00:00:21 +0100

    [rclone] Upadte to 1.68.2
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- rclone/plan.sh
+++ rclone/plan.sh
@@ -1,6 +1,6 @@
 pkg_name=rclone
 pkg_origin=rsertelon
-pkg_version=1.65.2
+pkg_version=1.68.2
 pkg_maintainer='Romain Sertelon <romain@sertelon.fr>'
 pkg_license='MIT'
 pkg_upstream_url='https://rclone.org/'
@@ -8,7 +8,7 @@ pkg_description='Rclone syncs your files to cloud storage'
 pkg_dirname="rclone-v${pkg_version}-linux-amd64"
 pkg_filename="${pkg_dirname}.zip"
 pkg_source="https://downloads.rclone.org/v${pkg_version}/${pkg_filename}"
-pkg_shasum="2155ea2c225272a6f78b2aa4547bb587c40b007586e73b41b31c59edba64f8fe"
+pkg_shasum="0e6fa18051e67fc600d803a2dcb10ddedb092247fc6eee61be97f64ec080a13c"
 pkg_bin_dirs=(bin)
 
 do_build() {
