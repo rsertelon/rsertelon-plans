f95eec4e55012b84044b9d4e04fd7d6c61d659c0
Romain Sertelon <romain@sertelon.fr>
2024-12-08 23:31:11 +0100

    [h5ai] Update source download url
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- h5ai/plan.sh
+++ h5ai/plan.sh
@@ -5,7 +5,7 @@ pkg_maintainer="Romain Sertelon <romain@sertelon.fr>"
 pkg_license=('MIT')
 pkg_description="modern HTTP web server index"
 pkg_upstream_url="https://larsjung.de/h5ai/"
-pkg_source="https://release.larsjung.de/h5ai/h5ai-${pkg_version}.zip"
+pkg_source="https://github.com/lrsjng/h5ai/releases/download/v${pkg_version}/h5ai-${pkg_version}.zip"
 pkg_shasum="fe138305a61d72094beb8c507dab728020158e4b00dae695a60b9803babb3e30"
 pkg_dirname="_h5ai"
 
