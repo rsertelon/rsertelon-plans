40d0ed6b02c3cb730fbd4c85c41cb48673e52850
Romain Sertelon <romain@sertelon.fr>
2025-09-14 00:08:24 +0200

    [ampache] Update to 6.6.7
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- ampache/plan.sh
+++ ampache/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=ampache
 pkg_origin=rsertelon
-pkg_version="6.6.4"
+pkg_version="6.6.7"
 pkg_maintainer="Romain Sertelon <romain@sertelon.fr>"
 pkg_license=("AGPL-3.0")
 pkg_upstream_url="http://ampache.org/"
 pkg_description="A web based audio/video streaming application and file manager allowing you to access your music & videos from anywhere, using almost any internet enabled device."
 pkg_source="https://github.com/ampache/ampache/archive/${pkg_version}.tar.gz"
-pkg_shasum="c721d0ebf8e7bf943b163cb5c7588bb760fe68352c04813c0af40ca0aa9bf4f1"
+pkg_shasum="dbb2ac0d7f0ebc5159a577d307945aff54a04e508efef800b31a938f9c175f09"
 pkg_filename="${pkg_version}.tar.gz"
 
 pkg_svc_user=root
