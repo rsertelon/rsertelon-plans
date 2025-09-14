08cbb9773cb62ba5802d7fc3858dd83cfa937c2d
Romain Sertelon <romain@sertelon.fr>
2025-09-14 22:33:03 +0200

    [libowfat] Update to 0.34
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- libowfat/plan.sh
+++ libowfat/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=libowfat
 pkg_origin=rsertelon
-pkg_version="0.32"
+pkg_version="0.34"
 pkg_upstream_url="http://www.fefe.de/libowfat/"
 pkg_description="Reimplement libdjb"
 pkg_maintainer="Romain Sertelon <romain@sertelon.fr>"
 pkg_license=('GPL-2.0')
 pkg_source="http://www.fefe.de/libowfat/libowfat-${pkg_version}.tar.xz"
-pkg_shasum="f4b9b3d9922dc25bc93adedf9e9ff8ddbebaf623f14c8e7a5f2301bfef7998c1"
+pkg_shasum="d4330d373ac9581b397bc24a22ad1f7f5d58a7fe36d9d239fe352ceffc5d304b"
 pkg_build_deps=(
   core/make
   core/gcc
@@ -21,5 +21,8 @@ do_build() {
 
 do_install() {
   cp $HAB_CACHE_SRC_PATH/$pkg_dirname/libowfat.a ${pkg_prefix}/lib/
+
+  mkdir -p ${pkg_prefix}/include/libowfat
   cp $HAB_CACHE_SRC_PATH/$pkg_dirname/*.h ${pkg_prefix}/include/
+  cp $HAB_CACHE_SRC_PATH/$pkg_dirname/*.h ${pkg_prefix}/include/libowfat/
 }
