b3c1800458233102f95be607caf65efb41bd5376
Romain Sertelon <romain@sertelon.fr>
2025-09-14 23:00:53 +0200

    [boost*] Update to 1.89.0
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- boost-build/plan.sh
+++ boost-build/plan.sh
@@ -2,14 +2,15 @@ pkg_name=boost-build
 pkg_origin=rsertelon
 pkg_description='Boost.Build is a text-based system for developing, testing, and installing software'
 pkg_upstream_url='http://www.boost.org/'
-pkg_version=1.80.0
+pkg_version=1.89.0
 pkg_maintainer='Romain Sertelon <romain@sertelon.fr>'
 pkg_license=('Boost Software License')
 pkg_dirname="boost_${pkg_version//./_}"
-pkg_source="https://boostorg.jfrog.io/artifactory/main/release/${pkg_version}/source/${pkg_dirname}.tar.gz"
-pkg_shasum="4b2136f98bdd1f5857f1c3dea9ac2018effe65286cf251534b6ae20cc45e1847"
+pkg_source="https://archives.boost.io/release/${pkg_version}/source/${pkg_dirname}.tar.gz"
+pkg_shasum="9de758db755e8330a01d995b0a24d09798048400ac25c03fc5ea9be364b13c93"
 
 pkg_deps=(
+  core/coreutils
   core/glibc
   core/gcc-libs
 )
@@ -22,6 +23,8 @@ pkg_bin_dirs=(bin)
 
 do_build() {
   pushd "tools/build"
+    fix_interpreter "src/engine/build.sh" core/coreutils bin/env
+
     ./bootstrap.sh
   popd
 }
--- boost/plan.sh
+++ boost/plan.sh
@@ -2,14 +2,15 @@ pkg_name=boost
 pkg_origin=rsertelon
 pkg_description='Boost provides free peer-reviewed portable C++ source libraries.'
 pkg_upstream_url='http://www.boost.org/'
-pkg_version=1.80.0
+pkg_version=1.89.0
 pkg_maintainer='Romain Sertelon <romain@sertelon.fr>'
 pkg_license=('Boost Software License')
 pkg_dirname="boost_${pkg_version//./_}"
-pkg_source="https://boostorg.jfrog.io/artifactory/main/release/${pkg_version}/source/${pkg_dirname}.tar.gz"
-pkg_shasum="4b2136f98bdd1f5857f1c3dea9ac2018effe65286cf251534b6ae20cc45e1847"
+pkg_source="https://archives.boost.io/release/${pkg_version}/source/${pkg_dirname}.tar.gz"
+pkg_shasum="9de758db755e8330a01d995b0a24d09798048400ac25c03fc5ea9be364b13c93"
 
 pkg_deps=(
+  core/coreutils
   core/glibc
   core/gcc-libs
   core/zlib
@@ -34,6 +35,8 @@ pkg_lib_dirs=(lib)
 pkg_include_dirs=(include)
 
 do_build() {
+  fix_interpreter "tools/build/src/engine/build.sh" core/coreutils bin/env
+
   ./bootstrap.sh --prefix="$pkg_prefix"
 }
 
