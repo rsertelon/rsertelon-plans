bddf0e79dcfeab111fc45cbcd0cce7433352c721
Romain Sertelon <romain@sertelon.fr>
2026-05-21 22:45:10 +0200

    [dolibarr] Update to 23.0.3
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- dolibarr/plan.sh
+++ dolibarr/plan.sh
@@ -1,12 +1,12 @@
 pkg_origin=rsertelon
 pkg_name=dolibarr$index
-pkg_version="23.0.2"
+pkg_version="23.0.3"
 pkg_description="Dolibarr ERP & CRM is a modern software package that helps manage your organization's activity (contacts, suppliers, invoices, orders, stocks, agenda…)."
 pkg_upstream_url="https://www.dolibarr.org/"
 pkg_maintainer="Romain Sertelon <romain@sertelon.fr>"
 pkg_license=("GPL-3.0-or-later")
 pkg_source="https://github.com/Dolibarr/dolibarr/archive/refs/tags/${pkg_version}.tar.gz"
-pkg_shasum="10754a727b2d82a5aa3881d221ffdfdd6f0b265761b7e6b016a6b588bcea7fe1"
+pkg_shasum="01e6fa4631ba6cc6aa5843906fa3a92b016d3280f8c924fce5aba164d29fdb72"
 pkg_dirname=dolibarr-${pkg_version}
 pkg_filename="dolibarr-${pkg_version}.tar.gz"
 
@@ -28,4 +28,8 @@ do_build(){
 do_install() {
   mkdir -p "${pkg_prefix}/dolibarr/"
   cp -r * "${pkg_prefix}/dolibarr/"
+}
+
+do_strip() {
+  return 0
 }
