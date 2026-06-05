82d57a8f846268a4d6e47287b78640aa666431cd
Romain Sertelon <romain@sertelon.fr>
2026-06-06 00:18:26 +0200

    [certbot] Update to 5.6.0
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- certbot/plan.sh
+++ certbot/plan.sh
@@ -1,6 +1,6 @@
 pkg_name=certbot
 pkg_origin=rsertelon
-pkg_version=3.0.1
+pkg_version=5.6.0
 pkg_maintainer='Romain Sertelon <romain@sertelon.fr>'
 pkg_license='Apache-2.0'
 pkg_upstream_url='https://certbot.eff.org'
