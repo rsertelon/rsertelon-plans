760fba3e3b9ecbdfc2606958c9ccbcadf0660884
Romain Sertelon <romain@sertelon.fr>
2026-06-06 00:09:15 +0200

    [libmilter] Update to 8.18.2
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- libmilter/plan.sh
+++ libmilter/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=libmilter
 pkg_origin=rsertelon
-pkg_version="8.18.1"
+pkg_version="8.18.2"
 pkg_upstream_url="https://www.proofpoint.com/us/products/open-source-email-solution"
 pkg_description="The Sendmail milter library"
 pkg_maintainer="Romain Sertelon <romain@sertelon.fr>"
 pkg_license=('Sendmail')
 pkg_source="ftp://ftp.sendmail.org/pub/sendmail/sendmail.${pkg_version}.tar.gz"
-pkg_shasum="cbf1f309c38e4806f7cf3ead24260f17d1fe8fb63256d13edb3cdd1a098f0770"
+pkg_shasum="1a085faa8ace52cffde2f5e9bc611bdb5f81481caaabf46f0437b719ca089d2f"
 pkg_dirname="sendmail-${pkg_version}"
 
 pkg_deps=(
