e03c9625a351497e7afe69e394f85d79c5fa4c8a
Romain Sertelon <romain@sertelon.fr>
2026-08-10 21:43:31 +0200

    [baikal] Update to 0.12.1
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- baikal/plan.sh
+++ baikal/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=baikal
 pkg_origin=rsertelon
-pkg_version="0.11.1"
+pkg_version="0.12.1"
 pkg_maintainer="Romain Sertelon <romain@sertelon.fr>"
 pkg_license=("GPL-3.0")
 pkg_description="Baïkal is a Cal and CardDAV server, based on sabre/dav, that includes an administrative interface for easy management."
 pkg_upstream_url="http://sabre.io/baikal/"
 pkg_source="https://github.com/sabre-io/Baikal/releases/download/${pkg_version}/baikal-${pkg_version}.zip"
-pkg_shasum="124a83adfdd29b7a2b48ce42ffcbdd39906ea0980bb98f79c8479b4a80647100"
+pkg_shasum="0449abb72b151d39d9c08c63cb83a05d9e9adb065b1165ef6786b0b6a13d203c"
 pkg_dirname="baikal"
 
 pkg_svc_user=root
