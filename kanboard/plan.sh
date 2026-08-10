6da5eb66fe69b190dedadd9cc267c16cc7b07e92
Romain Sertelon <romain@sertelon.fr>
2026-08-10 21:45:00 +0200

    [kanboard] Update to 1.2.53
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- kanboard/plan.sh
+++ kanboard/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=kanboard
 pkg_origin=rsertelon
-pkg_version="1.2.52"
+pkg_version="1.2.53"
 pkg_maintainer="Romain Sertelon <romain@sertelon.fr>"
 pkg_license=("MIT")
 pkg_upstream_url="https://kanboard.org/"
 pkg_description="Kanboard is a free and open source Kanban project management software."
 pkg_source="https://github.com/kanboard/kanboard/archive/refs/tags/v${pkg_version}.tar.gz"
-pkg_shasum="f5bc4d00489ae9b6e98609251a243b9b32ca9bdf29340bde1014ec26ad575660"
+pkg_shasum="12ad42a26c7eecba1d156fa6b05af0575e07232bb72fad64b232097df6e9ec9f"
 
 pkg_svc_user=root
 pkg_svc_group=$pkg_svc_user
