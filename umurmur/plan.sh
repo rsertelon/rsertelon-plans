c1ae018121a9faf970741a9d1701bd2a17482f76
Romain Sertelon <romain@sertelon.fr>
2026-08-25 00:23:32 +0200

    [umurmur] Update to 0.4.1
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- umurmur/plan.sh
+++ umurmur/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=umurmur
 pkg_origin=rsertelon
-pkg_version="0.3.1"
+pkg_version="0.4.1"
 pkg_maintainer="Romain Sertelon <romain@sertelon.fr>"
 pkg_license=("BSD-3-Clause")
 pkg_upstream_url="https://umurmur.net/"
 pkg_description="A minimalistic Mumble server primarily targeted to run on embedded computers"
 pkg_source="https://github.com/umurmur/umurmur/archive/v${pkg_version}.tar.gz"
-pkg_shasum="8327dd0b2c5bd187a38d098295e896a6b85d698c9268205bcb27f6244f760a73"
+pkg_shasum="e54e302a31d7ff4197ea6c77742b791d6ea508e0c60c955795f5195f0bec2edc"
 
 pkg_build_deps=(
   core/cmake
@@ -14,12 +14,12 @@ pkg_build_deps=(
 )
 pkg_deps=(
   core/cacerts
-  core/openssl11
+  core/openssl
   core/protobuf-c
   rsertelon/libconfig
 )
 
-pkg_bin_dirs=(sbin)
+pkg_bin_dirs=(bin)
 
 pkg_svc_user="hab"
 pkg_svc_group="hab"
@@ -28,7 +28,7 @@ do_build() {
   cmake -B builddir \
     -DCMAKE_INSTALL_PREFIX=$pkg_prefix \
     -DSSL=openssl \
-    -DOPENSSL_ROOT_DIR=$(pkg_path_for "core/openssl11") \
+    -DOPENSSL_ROOT_DIR=$(pkg_path_for "core/openssl") \
     -DLIBCONFIG_INCLUDE_DIR=$(pkg_path_for "rsertelon/libconfig")/include \
     -DLIBCONFIG_LIBRARIES=$(pkg_path_for "rsertelon/libconfig")/lib/libconfig.so \
     -DPROTOBUFC_INCLUDE_DIR=$(pkg_path_for "core/protobuf-c")/include \
