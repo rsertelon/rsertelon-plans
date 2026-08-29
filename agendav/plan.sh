69475b2d86d9fa811cde7bf67fc27123c547af05
Romain Sertelon <romain@sertelon.fr>
2026-08-30 01:07:21 +0200

    [agendav] Update to 3.3.1
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- agendav/plan.sh
+++ agendav/plan.sh
@@ -1,18 +1,19 @@
 pkg_name=agendav
 pkg_origin=rsertelon
-pkg_version="2.6.0"
+pkg_version="3.3.1"
 pkg_maintainer="Romain Sertelon <romain@sertelon.fr>"
 pkg_license=("GPL-3.0")
 pkg_description="AgenDAV is an open source multilanguage CalDAV web client which features a rich AJAX interface with shared calendars support"
 pkg_upstream_url="https://github.com/agendav/agendav"
 pkg_source="https://github.com/agendav/agendav/releases/download/${pkg_version}/agendav-${pkg_version}.tar.gz"
-pkg_shasum="af72c07886e30d2443ce9587992f59c78fad5ab4a069e311286e1e7216db895e"
+pkg_shasum="aec1038845ea9b489f575028d092fb7ef3dbee7dcf302783a3f6d383e7473030"
 
 pkg_svc_user=root
 pkg_svc_group=$pkg_svc_user
 
 pkg_deps=(
   core/nginx
+  rsertelon/php-85
 )
 
 pkg_build_deps=(
@@ -23,10 +24,10 @@ pkg_binds=(
   [php]="address port"
 )
 
-do_build(){
-  # Patch to fix a bug where agendav is unable to fetch and display events
-  # https://github.com/agendav/agendav/issues/289
-  patch --binary -p1 -i "$PLAN_CONTEXT"/patches/000_date_helper_error_while_fetching_events.patch
+do_build() {
+  # TODO: replace shebang php in bin/agendavcli to use rsertelon/php-85 php.
+  php_path="$(pkg_path_for rsertelon/php-85)/bin/php"
+  sed -i 's@#!/usr/bin/php@#!'"$php_path"'@' bin/agendavcli
 
   return 0
 }
@@ -34,4 +35,8 @@ do_build(){
 do_install() {
   mkdir -p "${pkg_prefix}/agendav/"
   cp -r * "${pkg_prefix}/agendav/"
+}
+
+do_strip() {
+  return 0
 }
