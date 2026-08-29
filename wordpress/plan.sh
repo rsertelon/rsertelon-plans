ee4ccc0d8353495598cdae57bdebc4b647579897
Romain Sertelon <romain@sertelon.fr>
2026-08-30 01:35:08 +0200

    [wordpress] Update to 6.9.7
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- wordpress/plan.sh
+++ wordpress/plan.sh
@@ -1,13 +1,13 @@
 pkg_origin=rsertelon
-pkg_name=wordpress
-pkg_version="6.9.4"
+pkg_name=wordpress$index
+pkg_version="6.9.7"
 pkg_description="Blog Tool, Publishing Platform and CMS"
 pkg_upstream_url="https://www.wordpress.org/"
 pkg_maintainer="Romain Sertelon <romain@sertelon.fr>"
 pkg_license=("GPL-2.0")
-pkg_source="https://wordpress.org/${pkg_name}-${pkg_version}.tar.gz"
-pkg_shasum="db610ad9f549e0abb5af3e3d5c6729c58d874439654cdf0afea425b2ea895f35"
-pkg_dirname=$pkg_name
+pkg_source="https://wordpress.org/wordpress-${pkg_version}.tar.gz"
+pkg_shasum="11fd65eccbf4dd5de5ecb3724b55f92c56d6c1ea967497fe9374bcdd7a6fbda6"
+pkg_dirname=wordpress
 
 pkg_svc_user=root
 pkg_svc_group=$pkg_svc_user
@@ -30,3 +30,6 @@ do_install() {
   cp -r * "${pkg_prefix}/wordpress/"
 }
 
+do_strip() {
+  return 0
+}
