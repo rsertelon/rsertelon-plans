229c39ff848ae19994fbbf9c121db963a511a4f8
Romain Sertelon <romain@sertelon.fr>
2026-08-30 02:36:12 +0200

    [wordpress] Update to 7.1
    Signed-off-by: Romain Sertelon <romain@sertelon.fr>

--- wordpress/plan.sh
+++ wordpress/plan.sh
@@ -1,12 +1,12 @@
 pkg_origin=rsertelon
 pkg_name=wordpress$index
-pkg_version="6.9.7"
+pkg_version="7.1"
 pkg_description="Blog Tool, Publishing Platform and CMS"
 pkg_upstream_url="https://www.wordpress.org/"
 pkg_maintainer="Romain Sertelon <romain@sertelon.fr>"
 pkg_license=("GPL-2.0")
 pkg_source="https://wordpress.org/wordpress-${pkg_version}.tar.gz"
-pkg_shasum="11fd65eccbf4dd5de5ecb3724b55f92c56d6c1ea967497fe9374bcdd7a6fbda6"
+pkg_shasum="05a5f89138f632b7329f1202f2a0553c5f7fe4daf8e4b9ca7ebae9b9466b9e86"
 pkg_dirname=wordpress
 
 pkg_svc_user=root
